e5c0b77aaef9d8e871edb9d5863440bf0a5f7fe5
Martin Coronel <martincxx@gmail.com>
2024-01-12 19:30:19 +0200

    Add scroll up action

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,14 @@ const blurHeader = () => {
     : header.classList.remove("blur-header");
 };
 window.addEventListener("scroll", blurHeader);
+
+// SHOW SCROLL UP
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // when the scroll is higher than 350v, add the show-scroll class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
